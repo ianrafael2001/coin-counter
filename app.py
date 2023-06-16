6723405ccece7e82b0799e7e21ad1fe44fa579f7
Ian Rafael <ianfael@gmail.com>
2023-06-16 02:45:35 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+# import cv2
+# from keras.models import load_model
+# import numpy as np
+
+
+def pre_process():
+    pass
+
+def edge_detection():
+    pass
+
+def feature_extraction():
+    pass
+
+def coin_classification():
+    pass
+
+def coin_counter():
+    pass
+
+def show_result():
+    pass
+
+while True:
+    pass
