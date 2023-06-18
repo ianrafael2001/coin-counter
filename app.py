857140212f3d3f51772d9905130ea70c1db46b19
Ian Rafael <ianfael@gmail.com>
2023-06-18 05:22:10 -0300

    some changes

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import cv2
 import numpy as np
+from keras.models import load_model
+
+
+video = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+model = load_model('Keras_model.h5',compile=False)
+data = np.ndarray(shape=(1,224,224,3),dtype=np.float32)
+classes = ["1 real","25 centavos","50 centavos"]
 
 '''
     Aplica técnicas de melhoria de qualidade da imagem, como 
@@ -7,13 +15,9 @@ import numpy as np
     visando obter uma imagem mais adequada para a detecção das moedas.
 '''
 def pre_process(image):
-    
-    # Redimensionamento da imagem
-    resized_image = cv2.resize(image, (800, 600))
-
     # Correção de iluminação
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     enhanced_image = clahe.apply(gray_image)
 
     # Filtragem de ruído
@@ -33,9 +37,9 @@ def edge_detection(image):
     return edges_canny
 
 '''
-    Calcula características relevantes das moedas segmentadas,
-    como diâmetro, área ou perímetro. Essas características
-    são utilizadas para distinguir as diferentes moedas.
+    algoritmos de detecção de bordas, como o operador de Sobel 
+    ou o algoritmo de Canny, para identificar as bordas das moedas 
+    na imagem pré-processada.
 '''
 def coin_segmentation(edges):    
     # Aplica a limiarização para segmentar as moedas
@@ -46,9 +50,10 @@ def coin_segmentation(edges):
 
     # Cria uma máscara para destacar as moedas
     mask = cv2.drawContours(np.zeros_like(edges), contours, -1, (255), thickness=cv2.FILLED)
-
-    # Aplica a máscara na imagem original para obter somente as moedas
-    segmented_image = cv2.bitwise_and(edges, edges, mask=mask)
+    
+    kernel = create_circular_kernel(5)
+    mask = cv2.dilate(mask,kernel,iterations=5)
+    segmented_image = cv2.erode(mask,kernel,iterations=5)
     return segmented_image
 
 '''
@@ -65,31 +70,17 @@ def feature_extraction():
     Essa etapa utiliza técnicas como classificação por máquina de vetores
     de suporte (SVM), árvores de decisão ou redes neurais.
 '''
-def coin_classification():
-    pass
+def coin_classification(coin_image):
+    coin_image = cv2.resize(coin_image,(224,224))
+    coin_image = np.asarray(coin_image)
+    coin_image_normalize = (coin_image.astype(np.float32)/127.0)-1
+    data[0] = coin_image_normalize
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    percent = prediction[0][index]
+    classe = classes[index]
+    return classe,percent
 
-'''
-    O modelo de classificação é aplicado para
-    classificar cada moeda e contar quantas vezes cada classe de
-    moeda aparece na imagem.
-'''
-def coin_counter(segmented_image):
-    # Encontra os contornos das moedas na imagem segmentada
-    contours, _ = cv2.findContours(segmented_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Desenha os contornos das moedas na imagem original
-    coins_image = cv2.cvtColor(segmented_image, cv2.COLOR_GRAY2BGR)
-    cv2.drawContours(coins_image, contours, -1, (0, 255, 0), 2)
-
-    # Conta o número de moedas
-    num_coins = len(contours)
-
-    # Exibe a imagem com as áreas das moedas
-    cv2.imshow("Coins", coins_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    return num_coins
 
 ''' 
     Os resultados da contagem das moedas são apresentados
@@ -97,38 +88,61 @@ def coin_counter(segmented_image):
     as moedas identificadas.
 '''
 def show_result():
-    pass
+    while True:
+        _,image = video.read()
+        image = cv2.resize(image,(640,480))
+        preprocessed_image = pre_process(image)
+        edges_canny = edge_detection(preprocessed_image)
+        segmented_image = coin_segmentation(edges_canny)
+        
+        # Encontra os contornos das moedas na imagem segmentada
+        contours, _ = cv2.findContours(segmented_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        #filtra o contorno pelo tamanho da area
+        contoursFilter = list(filter(filterByArea, contours))    
+        
+        # Desenha os contornos filtrados na imagem segmentada
+        coins_image = cv2.cvtColor(segmented_image, cv2.COLOR_GRAY2BGR)
+        cv2.drawContours(coins_image, contoursFilter, -1, (0, 255, 0), 2)
+        
+        # Conta o número de moedas    
+        num_coins = len(contoursFilter)
+        
+        cv2.putText(image,f'Quantidade de moedas: {num_coins}',(0,30),cv2.FONT_HERSHEY_SIMPLEX,1.2,(255,255,255),2)
+        
+        for cnt in contoursFilter:
+            x,y,w,h = cv2.boundingRect(cnt)
+            cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+            recorte = image[y:y +h,x:x+ w]
+            
+            # faz a classificação da moeda
+            classe, accuracy = coin_classification(recorte)
+            
+            accuracy_form = float("{:.2f}".format(accuracy))
+            
+            cv2.putText(image,str(classe),(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
+            cv2.putText(image,f'{int(accuracy_form*100)}%',(x ,y - 25),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0),2)
+                
+        cv2.imshow('Imagem Original',image)
+        cv2.imshow('Imagem seguimentada', coins_image)
+        cv2.waitKey(1)
+        pass
 
+def create_circular_kernel(radius):
+    diameter = 2 * radius + 1
+    kernel = np.zeros((diameter, diameter), dtype=np.uint8)
+    center = (radius, radius)
 
-image = cv2.imread('./assets/image.jpg')
+    for i in range(diameter):
+        for j in range(diameter):
+            distance = np.sqrt((i - center[0]) ** 2 + (j - center[1]) ** 2)
+            if distance <= radius:
+                kernel[i, j] = 1
 
-preprocessed_image = pre_process(image)
+    return kernel
 
-cv2.imshow("preprocessed_image", preprocessed_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def filterByArea(cnt):
+   area = cv2.contourArea(cnt)
+   return area > 1000
 
-edges_canny = edge_detection(preprocessed_image)
-
-cv2.imshow("preprocessed_image", edges_canny)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-segmented_image = coin_segmentation(edges_canny)
-
-cv2.imshow("segmented_image", segmented_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# feature_extraction()
-
-# coin_classification()
-
-num_coins = coin_counter(segmented_image)
-
-print("Número de moedas encontradas:", num_coins)
-
-
-# while True:
-#     pass
+show_result()
